6f520d752d15f94558f522d2f2e787b15c798207
changjichun3 <changjichun@jd.com>
2021-05-26 15:01:28 +0800

    init

---
+++ src/main/java/com/vincent/lowcode/LowcodeApplication.java
@@ -0,0 +1,13 @@
+package com.vincent.lowcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LowcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vincent/lowcode/LowcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.lowcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
